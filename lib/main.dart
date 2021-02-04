9d0bcc65c08e601fc2b393a43c52d219e8b72246
Richard Robinson Campos <rrobinson.campos@gmail.com>
2021-02-04 11:00:30 -0300

    Refactor commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,10 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:services_app/src/app.dart';
+import 'package:services_app/app/app_module.dart';
+import 'package:flutter_modular/flutter_modular.dart';
 
-void main() {
-  runApp(App());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(ModularApp(module: AppModule()));
 }
